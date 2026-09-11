1c1bd6be5b5d53a16f12dea72fda3fb3caaed91d
Abhishek-Savita-3012 <asavita3012@gmail.com>
2026-09-11 16:40:36 +0530

    Initial Spring Boot project setup

--- src/main/java/com/abhishek/job_tracker/controller/HealthController.java
+++ src/main/java/com/abhishek/job_tracker/controller/HealthController.java
@@ -1,12 +1,14 @@
 package com.abhishek.job_tracker.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class JobController {
-    @GetMapping("/api/health")
+@RequestMapping("/api")
+public class HealthController {
+    @GetMapping("/health")
     public String health() {
-        return "status: Ok" + "\n" + "application: Job Tracker Application";
+        return "Job Tracker API is running";
     }
 }
